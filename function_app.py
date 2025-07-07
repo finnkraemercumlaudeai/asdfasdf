65560f4bfbf9e45f38fb01616daff25d53abc3b3
finnkraemercumlaudeai <finn.kraemer@cumlaude.ai>
2025-07-07 13:27:57 +0200

    addTestDemoHttpFunction

---
+++ function_app.py
@@ -0,0 +1,27 @@
+import azure.functions as func
+import datetime
+import json
+import logging
+
+app = func.FunctionApp()
+
+@app.route(route="TestHttpGetLenferink", auth_level=func.AuthLevel.ANONYMOUS)
+def TestHttpGet(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. The Lenferink demo is ready.",
+             status_code=200
+        )
